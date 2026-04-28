82cd6457d8abd488e41a3cd3ebd3078558b7f0aa
Tommi Valikangas <tvalikan@puhti-login11.bullx>
2026-04-28 09:08:36 +0300

    Updated rRNA filtering scripts

---
+++ scripts/3b_qc_rrna_filt_reads.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#SBATCH --job-name=fastqc_cutadapt
+#SBATCH --error=3b_qc_trim_rrna_filt_%A_err.txt
+#SBATCH --output=3b_qc_trim_rrna_filt_%A_out.txt
+#SBATCH --partition=small
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=64G
+#SBATCH --time=04:00:00
+#SBATCH --account=project_2009164
+
+# a script to QC th rRNA filtered metatranscriptomics sequencing files
+
+# define the needed directories
+# this is the project root directory under which all the subdirectories, results etc. are created.
+project_root="/scratch/project_2009164/2_OULANKA/Tommi/final_review" # e.g. "/scratch/project_number/puukkosuo"
+
+# the main folder, where all the metatranscriptomics analysis steps and results will be stored
+mt_main_dir="$project_root/metatranscriptomics"
+
+# the directory that contains the rRNA filtered metatranscriptomis data
+mt_raw_data_dir="$project_root/metatranscriptomics/rrna_filtered_trimmed_fastq" # e.g. "/scratch/project_number/RAW/metatranscriptomics"
+
+# sample name file for the metatranscriptomics samples - prepared by the 1_SETUP script
+mt_sample_name_file="$project_root/metadata/MT_Sample_Names.txt"
+
+# perform fastqc for the fastq files
+module load biokit/11.3.0 # fastqc 0.11.9 is used
+
+echo "[$(date -Is)] Running Fastqc and Multiqc for the metatranscriptomics rRNA filtered fastq files..."
+
+# create the directory
+mkdir -p "$mt_main_dir/fastqc/cutadapt_trimmed/rrna_filtered" 
+cd "$mt_raw_data_dir"
+for j in * 
+do
+	fastqc --noextract -t "$SLURM_CPUS_PER_TASK" -o "$mt_main_dir/fastqc/cutadapt_trimmed/rrna_filtered/" "$j"
+done
+
+# perform multiqc for the fastqc results
+module purge
+module load multiqc/1.19
+cd "$mt_main_dir/fastqc/cutadapt_trimmed/rrna_filtered"
+multiqc .
+echo "*******************************************************"
+echo "*******************************************************"
+echo "[$(date -Is)] Metatranscriptomics samples processed. Everything done. Bye for now."
